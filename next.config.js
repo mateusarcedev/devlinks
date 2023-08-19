309993849a2dd22cd5eb15b835a113f57a623c0d
Mateus Arce <arcemateuss@gmail.com>
2023-08-19 18:34:53 -0400

    modificando o nextjs.config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'standalone',
+  compress: false,
+  images: {
+    unoptimized: true
+  }
+}
 
 module.exports = nextConfig
