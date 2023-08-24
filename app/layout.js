cad40ea3f9d14a6736f22517be634970e643c7e0
Mateus Arce <arcemateuss@gmail.com>
2023-08-24 12:29:50 -0400

    Adicionando metatags

--- app/layout.js
+++ app/layout.js
@@ -3,34 +3,36 @@ import { Inter } from 'next/font/google'
 import Head from 'next/head';
 
 
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'DevLinks',
   description: 'um projeto criado para ajudar programadores a encontrar ferramentas úteis para o desenvolvimento de softwares. Aqui você encontrará materiais para desenvolvimento e estudo de frontend, backend e estruturas de dados.',
+  url: 'https://devlinks.mateusarce.dev/',
+  siteName: 'DevLinks',
+  image: '/favicon.icon',
+  locale: 'pt_BR',
+  type: 'website'
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
       <Head>
-        <script
-          type="text/javascript"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "ijb88pvali");
-            `,
-          }}
-        />
-        <script
-          defer
-          data-domain="devlinks.mateusarce.dev"
-          src="https://plausible.io/js/script.js"
-        />
+        <meta property="og:title" content="DevLinks" />
+        <meta property="og:description" content="um projeto criado para ajudar programadores a encontrar ferramentas úteis para o desenvolvimento de softwares. Aqui você encontrará materiais para desenvolvimento e estudo de frontend, backend e estruturas de dados." />
+        <meta property="og:url" content="https://devlinks.mateusarce.dev/" />
+        <meta property="og:site_name" content="DevLinks" />
+        <meta property="og:locale" content="pt_BR" />
+        <meta property="og:image:url" content="/favicon.ico" />
+        <meta property="og:image:width" content="800" />
+        <meta property="og:image:height" content="600" />
+        <meta property="og:image:url" content="/favicon.ico" />
+        <meta property="og:image:width" content="1800" />
+        <meta property="og:image:height" content="1600" />
+        <meta property="og:image:alt" content="Image preview" />
+        <meta property="og:type" content="website" />
       </Head>
       <body className={inter.className}>{children}</body>
     </html>
