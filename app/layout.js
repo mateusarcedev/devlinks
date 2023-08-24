4ceabfbb978374483576c7f08daba03a6294b339
Mateus Arce <arcemateuss@gmail.com>
2023-08-24 07:51:20 -0400

    Adicionando ferramenta de analitycs - Microsoft Clarity

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Head from 'next/head';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,6 +13,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
+       <Head>
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "ijb88pvali");
+            `,
+          }}
+        />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
