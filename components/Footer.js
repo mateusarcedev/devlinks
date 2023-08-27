a64bb3f7403ffcebc2a423510d2de61bde9ec0cd
Mateus Arce <arcemateuss@gmail.com>
2023-08-27 18:56:13 -0400

    mudanças no footer

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 export default function Footer() {
   return (
-    <div className="flex align-center justify-center text-white text-end text-2xl mb-4">
+    <div className="flex align-center justify-center text-white text-2xl ">
       <Link href="https://www.mateusarce.dev/" target="_blank" >
         Criado por <strong className="text-blue-500 hover:text-blue-600">Mateus Arce</strong>
       </Link>
