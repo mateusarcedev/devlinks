363e2aefd365a11b81a660b0deb9622c6ff9683c
Mateus Arce <arcemateuss@gmail.com>
2023-09-15 18:09:39 -0400

    adicionando ferramentas de cores

--- app/colors/page.js
+++ app/colors/page.js
@@ -4,10 +4,10 @@ import Navbar from "@/components/Navbar";
 
 
 export default function Home() {
-  return (
-  <>
+return (
+<>
       <Navbar/>
-    <main className="bg-background flex flex-col items-center w-full ">
+      <main className="bg-background flex flex-col items-center w-full ">
       <div className="w-4/5 flex justify-center items-center">
       <CategoryBar />
       </div>
@@ -75,7 +75,7 @@ export default function Home() {
       title="Brand Colors" 
       src="/colors/brandcolors.png" 
       alt="Imagem do card" 
-      desc="reúne as cores oficiais de marcas famosas, como Facebook, Google, Twitter, entre outras. É uma referência útil para designers e profissionais de marketing que desejam utilizar as cores autênticas das marcas em seus projetos. O site oferece os códigos hexadecimais das cores para fácil utilização." 
+      desc="Reúne as cores oficiais de marcas famosas, como Facebook, Google, Twitter, entre outras. É uma referência útil para designers e profissionais de marketing que desejam utilizar as cores autênticas das marcas em seus projetos. O site oferece os códigos hexadecimais das cores para fácil utilização." 
       />
 
 <Card 
@@ -94,8 +94,56 @@ export default function Home() {
       desc="Este site oferece diversas paletas de cores monocromáticas com uma ampla gama de tons de uma única cor." 
       />
 
+<Card 
+      url="https://gradienthunt.com/"
+      title="Gradient Hunt" 
+      src="/colors/gradienthunt.png" 
+      alt="Imagem do card" 
+      desc="O Gradient Hunt é um site que oferece uma coleção de gradientes de cores bonitos. É uma ótima ferramenta para designers e desenvolvedores que desejam encontrar inspiração e usar gradientes em seus projetos. O site é fácil de navegar e permite que você copie os códigos de cores dos gradientes desejados." 
+      />
+
+<Card 
+      url="https://pigment.shapefactory.co/"
+      title="Pigment" 
+      src="/colors/pigmentui.png" 
+      alt="Imagem do card" 
+      desc="O Pigment é um site que oferece uma variedade de paletas de cores para inspiração e uso em projetos de design. Com uma interface simples e intuitiva, é fácil explorar e selecionar paletas de cores que se adequem às suas necessidades. O site também permite que você copie os códigos de cores das paletas escolhidas." 
+      />
+
+<Card 
+      url="https://gradientbuttons.colorion.co/"
+      title="Gradient Buttons" 
+      src="/colors/gradientbuttons.webp" 
+      alt="Imagem do card" 
+      desc="O Gradient Buttons é um site que oferece uma coleção de botões com gradientes de cores pré-definidos. É uma ferramenta útil para designers e desenvolvedores que desejam adicionar botões atraentes e estilizados em seus projetos. O site permite que você visualize e copie o código CSS dos botões gradientes selecionados." 
+      />
+
+<Card 
+      url="https://flatuicolors.com/"
+      title="Flat UI" 
+      src="/colors/flatuicolors.ico" 
+      alt="Imagem do card" 
+      desc="O Flat UI Colors 2 é um site que oferece 14 paletas de cores planas, totalizando 280 cores para uso em projetos de design. É uma ferramenta útil para encontrar combinações de cores modernas e vibrantes. O site permite que você copie os códigos de cores hexadecimais das paletas selecionadas." 
+      />
+
+<Card 
+      url="https://color.adobe.com/pt/create/color-wheel"
+      title="Adobe Color" 
+      src="/colors/adobecolors.ico" 
+      alt="Imagem do card" 
+      desc="O Adobe Color é um site que permite criar paletas de cores personalizadas usando a roda de cores. É uma ferramenta útil para designers que desejam explorar combinações de cores harmoniosas. O site oferece recursos avançados, como a capacidade de extrair paletas de cores de imagens." 
+      />
+
+<Card 
+      url="https://colorable.jxnblk.com/"
+      title="Colorable" 
+      src="/colors/colorable.png" 
+      alt="Imagem do card" 
+      desc="O Colorable é um site que ajuda a avaliar a legibilidade de combinações de cores para design de interfaces. Ele fornece informações sobre o contraste entre as cores e se atendem aos padrões de acessibilidade. É uma ferramenta útil para garantir que o texto seja facilmente legível em diferentes fundos coloridos." 
+      />
+
       </div>
-    </main>
-  </>
-  )
+</main>
+</>
+)
 }
