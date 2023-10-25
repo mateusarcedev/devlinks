392e962fd08b2abe61e7693cb10e7f380521641f
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-10-25 15:14:31 -0300

    Adiciona `openGraph` no objeto `metadata`
    Remove os `<meta />` tags com metadados de `open-graph` e move-os para o já existente objeto `metadata`
    
    https://nextjs.org/docs/app/api-reference/functions/generate-metadata#opengraph

--- app/layout.js
+++ app/layout.js
@@ -14,26 +14,35 @@ export const metadata = {
   siteName: 'DevLinks',
   image: '/favicon.icon',
   locale: 'pt_BR',
-  type: 'website'
+  type: 'website',
+  openGraph: {
+    title: 'DevLinks',
+    description: 'um projeto criado para ajudar programadores a encontrar ferramentas úteis para o desenvolvimento de softwares. Aqui você encontrará materiais para desenvolvimento e estudo de frontend, backend e estruturas de dados.',
+    url: 'https://devlinks.mateusarce.dev/',
+    siteName: 'DevLinks',
+    locale: 'pt_BR',
+    images: [
+      {
+        url: '/favicon.ico',
+        width: 800,
+        height: 600,
+        alt: 'Image Preview'
+      },
+      {
+        url: '/favicon.ico',
+        width: 1800,
+        height: 1600,
+        alt: 'Image Preview'
+      },
+    ],
+    type: 'website'
+  }
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br" className='h-full'>
-      <Head>
-        <meta property="og:title" content="DevLinks" />
-        <meta property="og:description" content="um projeto criado para ajudar programadores a encontrar ferramentas úteis para o desenvolvimento de softwares. Aqui você encontrará materiais para desenvolvimento e estudo de frontend, backend e estruturas de dados." />
-        <meta property="og:url" content="https://devlinks.mateusarce.dev/" />
-        <meta property="og:site_name" content="DevLinks" />
-        <meta property="og:locale" content="pt_BR" />
-        <meta property="og:image:url" content="/favicon.ico" />
-        <meta property="og:image:width" content="800" />
-        <meta property="og:image:height" content="600" />
-        <meta property="og:image:url" content="/favicon.ico" />
-        <meta property="og:image:width" content="1800" />
-        <meta property="og:image:height" content="1600" />
-        <meta property="og:image:alt" content="Image preview" />
-        <meta property="og:type" content="website" />
+      <Head>        
         <Script async src="https://analytics.umami.is/script.js" data-website-id="830f9d6c-9dd7-4db6-a4f3-ffda7a1fae26" />
       </Head>
       <body className={inter.className}>{children}</body>
