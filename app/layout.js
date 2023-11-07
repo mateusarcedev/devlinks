763101134631a3b6f23a29f4cff7665b74431067
Mateus Arce <arcemateuss@gmail.com>
2023-11-07 10:20:59 -0400

    change metadatas for openGraph

--- app/layout.js
+++ app/layout.js
@@ -8,13 +8,27 @@ import Script from 'next/script'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'DevLinks',
-  description: 'um projeto criado para ajudar programadores a encontrar ferramentas úteis para o desenvolvimento de softwares. Aqui você encontrará materiais para desenvolvimento e estudo de frontend, backend e estruturas de dados.',
-  url: 'https://devlinks.mateusarce.dev/',
-  siteName: 'DevLinks',
-  image: '/favicon.icon',
-  locale: 'pt_BR',
-  type: 'website'
+  openGraph: {
+    title: 'DevLinks',
+    description: 'Descubra ferramentas essenciais para programadores. Acelere seu desenvolvimento de software.',
+    url: 'https://devlinks.mateusarce.dev/',
+    siteName: 'DevLinks',
+    images: [
+      {
+        url: './devlinks.svg',
+        width: 800,
+        height: 600,
+      },
+      {
+        url: './devlinks.svg',
+        width: 1800,
+        height: 1600,
+        alt: 'Logo do Site',
+      },
+    ],
+    locale: 'pt_BR',
+    type: 'website',
+  },
 }
 
 export default function RootLayout({ children }) {
@@ -22,7 +36,7 @@ export default function RootLayout({ children }) {
     <html lang="pt-br" className='h-full'>
       <Head>
         <meta property="og:title" content="DevLinks" />
-        <meta property="og:description" content="um projeto criado para ajudar programadores a encontrar ferramentas úteis para o desenvolvimento de softwares. Aqui você encontrará materiais para desenvolvimento e estudo de frontend, backend e estruturas de dados." />
+        <meta property="og:description" content="Descubra ferramentas essenciais para programadores. Acelere seu desenvolvimento de software." />
         <meta property="og:url" content="https://devlinks.mateusarce.dev/" />
         <meta property="og:site_name" content="DevLinks" />
         <meta property="og:locale" content="pt_BR" />
@@ -34,7 +48,6 @@ export default function RootLayout({ children }) {
         <meta property="og:image:height" content="1600" />
         <meta property="og:image:alt" content="Image preview" />
         <meta property="og:type" content="website" />
-        <Script async src="https://analytics.umami.is/script.js" data-website-id="830f9d6c-9dd7-4db6-a4f3-ffda7a1fae26" />
       </Head>
       <body className={inter.className}>{children}</body>
     </html>
