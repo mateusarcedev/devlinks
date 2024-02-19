161faef2ea782830393650b4032ea73f050016e3
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2024-02-19 16:33:02 -0400

    fix: Ajustado componente Navbar

--- app/page.js
+++ app/page.js
@@ -2,30 +2,37 @@ import CategoryBar from "@/components/CategoryBar";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 
-
 export default function Home() {
   return (
-  <div className="h-screen flex flex-col justify-between">
-      <Navbar/>
-      
-      <div className="w-4/5 flex justify-center items-center mx-auto">
-      <CategoryBar />
-      </div>
-      <main className="bg-background flex flex-1 flex-col items-center w-full " >
-      <div className="w-4/5 flex justify-center items-center mt-4 gap-4 flex-wrap">
-      
-      <h1 className="text-[#D9D9D9] text-4xl">Sobre o projeto 🧑🏽‍💻</h1>
-      <p className="text-[#D9D9D9] text-xl">
-      Este é um projeto criado para ajudar programadores iniciantes a encontrar ferramentas úteis para o desenvolvimento de softwares. Aqui você encontrará materiais para desenvolvimento frontend, backend e estudo de estruturas de dados. Sinta-se livre para contribuir.
-      </p>
+    <div className="bg-background h-full flex flex-col">
+      <Navbar />
 
-      <h1 className="text-[#D9D9D9] text-4xl">De onde veio a ideia? 💡</h1>
-      <p className="text-[#D9D9D9] text-xl">
-      Quando comecei a estudar programação, senti uma grande dificuldade em encontrar materiais de estudo e ferramentas que ajudassem no desenvolvimento. Então decidi criar este repositório, reunindo informações de threads do Twitter, Instagram e outros sites para ajudar tanto programadores novos quanto experientes.
-      </p>
-      </div>
-    </main>
+      <main className="flex flex-col items-center px-24 py-16 gap-12 text-[#D9D9D9]">
+        <CategoryBar />
+        <div className="flex flex-col gap-4 items-center">
+          <h1 className="text-4xl">Sobre o projeto 🧑🏽‍💻</h1>
+          <p className="text-xl">
+            Este é um projeto criado para ajudar programadores iniciantes a
+            encontrar ferramentas úteis para o desenvolvimento de softwares.
+            Aqui você encontrará materiais para desenvolvimento frontend,
+            backend e estudo de estruturas de dados. Sinta-se livre para
+            contribuir.
+          </p>
+        </div>
+
+        <div className="w-4/5 flex flex-col gap-4 items-center">
+          <h1 className="text-4xl">De onde veio a ideia? 💡</h1>
+          <p className="text-xl">
+            Quando comecei a estudar programação, senti uma grande dificuldade
+            em encontrar materiais de estudo e ferramentas que ajudassem no
+            desenvolvimento. Então decidi criar este repositório, reunindo
+            informações de threads do Twitter, Instagram e outros sites para
+            ajudar tanto programadores novos quanto experientes.
+          </p>
+        </div>
+      </main>
+
       <Footer />
-  </div>
-  )
+    </div>
+  );
 }
